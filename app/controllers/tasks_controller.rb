5b95bb82d0abec4a136bd9672582ccbe8da8c854
Smith <smithwebdd@gmail.com>
2019-08-26 11:59:54 -0400

    updated form to include delete link, created destroy action for remote call, updated detroy action on controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create, :update]
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_task, only: [:edit, :update, :destroy]
 
   #index and show actions removed 
@@ -18,16 +18,6 @@ class TasksController < ApplicationController
   def create
     # @task = Task.new(task_params)
     @task = Task.create(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /tasks/1
@@ -40,10 +30,6 @@ class TasksController < ApplicationController
   # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
